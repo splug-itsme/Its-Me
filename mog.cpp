5887f5d1825b5eab72ee73564f6365e5c7f941ae
이유정 <dnfldml3476@gmail.com>
2017-07-28 13:41:16 +0900

    수정

--- mog.cpp
+++ mog.cpp
@@ -23,20 +23,65 @@
 using namespace cv;
 using namespace std;
 
+int sub_Bground(char *videoFile);
 void changeGray(Mat &Img);
 void cvDiff(Mat &image, Mat &image2, Mat &diff);
 void copy(const Mat &Img, Mat &result, Mat &mask);
-void checkDegree(vector <Mat> &frame, int start, int end);
-int sampleVal(vector <int> &agrDegree); 
+void cal_Degree(vector <Mat> &frame, int start, int end); 
+int return_Max(vector <int> &agrDegree); 
+void gray(char *videoFile);
 
 Mat des;
 
 int main(int argc, char *argv[])
 {
-//	CvCapture *capture = cvCaptureFromFile("mog3.avi");
-	VideoCapture capture("3.mp4");
+	//cvNamedWindow("Image");
+	//cvNamedWindow("Image2");
+	//cvNamedWindow("tmpImage");
+	//cvNamedWindow("diffImage");
+	//cvNamedWindow("resultImage");
+	//if (sub_Bground("3.mp4") == 1)
+	//	return 1;
+	//if (cvWaitKey() == 27) 
+	//	; // break에서 바꿈 키입력받을때마다 프레임이동
+
+	gray("3.mp4");
+	cvDestroyWindow("tt");
+	
+	return 0;
+}
+void gray(char *videoFile)
+{
+	VideoCapture capture(videoFile);  // 영상 파일 읽기
+	if (!capture.isOpened()) {
+		//error in opening the video input
+		cerr << "Unable to open video file: " << "tt" << endl;
+		exit(EXIT_FAILURE);
+	}
+	Mat Img = imread("result.bmp", IMREAD_GRAYSCALE);
+	Mat diff = Mat(Img.rows, Img.cols, CV_8UC1, Scalar(0));
+	Mat readImg = Mat(Img.rows, Img.cols, CV_8UC1, Scalar(0));
+	Mat tmp = Mat(Img.rows, Img.cols, CV_8UC1, Scalar(0));
+
+	while (capture.read(readImg)) {
+		cvtColor(readImg, readImg, CV_BGR2GRAY);
+		absdiff(Img, readImg, diff); // 첫 프레임과 매 프레임마다의 차영상을 diff에 저장
+		threshold(diff, tmp, 30, 255, CV_THRESH_BINARY); // 차영상을 이진화 시켜서 tmp에 저장
+		erode(tmp, tmp, Mat(), Point(-1, -1), 3);
+		imshow("tmp", tmp);
+		imshow("Img", Img);
+		imshow("readImg", readImg);
+		if (cvWaitKey() == 27) continue; // break에서 바꿈 키입력받을때마다 프레임이동
+
+	}
+
+}
+int sub_Bground(char *videoFile)
+{
+	VideoCapture capture(videoFile);  // 영상 파일 읽기
 	VideoWriter vw;
 	vw = VideoWriter("AA.mp4", CV_FOURCC('D', 'I', 'V', 'X'), capture.get(CV_CAP_PROP_FPS), Size((int)capture.get(CV_CAP_PROP_FRAME_WIDTH), (int)capture.get(CV_CAP_PROP_FRAME_HEIGHT)), true);
+	vector<Mat> src;
 
 	if (!vw.isOpened())
 	{
@@ -48,116 +93,83 @@ int main(int argc, char *argv[])
 		cerr << "Unable to open video file: " << "tt" << endl;
 		exit(EXIT_FAILURE);
 	}
-	cvNamedWindow("Image");
-	cvNamedWindow("Image2");
-	cvNamedWindow("Image3");
-	cvNamedWindow("tmpImage");
-	cvNamedWindow("diffImage");
-	cvNamedWindow("resultImage");
-	cvNamedWindow("tmp2");
-	
-	//Mat t;
-	//capture.read(t);
-	//imwrite("test.bmp", t);
-	//exit(0);
-	vector<Mat> src;
 
-	Mat Img = imread("result.bmp");
-	Mat Img2 = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0));
-	Mat Img3 = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0));
-	Mat Img4 = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0));
 
-	Mat res = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0));
+	Mat Img;
+	capture.read(Img); // 처프레임을 Img에 저장
+
+	//Mat Img = imread("result.bmp"); // Img에 결과로 얻은 이미지 저장
+	Mat readImg = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0)); // 
 	Mat diff = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0));
 	Mat tmp = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0));
-	Mat MOG = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0));
+	des = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0)); // 결과를 저장할 Mat
 
-	des = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0));
-	
 	Ptr<BackgroundSubtractor> pMOG2; //MOG2 Background subtractor
 	pMOG2 = createBackgroundSubtractorMOG2();
 	int check = 0;
 	vector <Mat> frame;
 
-	while (capture.read(Img2))
+	while (capture.read(readImg))
 	{
 		check++;
-		if (check == 0)
-			continue;
 		if (check % 5 != 0)
 			continue;
 		Mat result = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0));
-		
-		absdiff(Img, Img2, diff);
-		threshold(diff, tmp, 70 ,255, CV_THRESH_BINARY);
-		
-	
-		//cvtColor(tmp, tmp, CV_BGR2GRAY);
-		changeGray(tmp);
-		 
-		//Img2.copyTo(Img, tmp); // inputMat.copyTo(outputMat, maskMat);
 
-		
-		pMOG2->apply(tmp, diff);
-		erode(diff, diff, Mat(), Point(-1, -1), 3);
+		absdiff(Img, readImg, diff); // 첫 프레임과 매 프레임마다의 차영상을 diff에 저장
+		threshold(diff, tmp, 20, 255, CV_THRESH_BINARY); // 차영상을 이진화 시켜서 tmp에 저장
+
+														 //cvtColor(tmp, tmp, CV_BGR2GRAY);
+		changeGray(tmp); // 이진화 시킨 tmp를 완벽하게 이진화
+
+		pMOG2->apply(tmp, diff); // 이진화 시킨 것을 가지고 MOG2
+		erode(diff, diff, Mat(), Point(-1, -1), 3); 
 		//blur(diff, diff, Size(5, 5)); // noise 제거
 		diff = ~diff;
-		Img2.copyTo(result, diff);
-	//	tmp = tmp & diff;
-
+		readImg.copyTo(result, diff); // 물체 영역의 반전을 붙여넣는다.
 
 
 		frame.push_back(result);
-		
-	
-	/*	if (frame.size() == 10) {
-			checkDegree(frame);
-			src.push_back(Img3);
-			frame.clear();
-		}*/	
-		
+
+		/*	if (frame.size() == 10) {
+		checkDegree(frame);
+		src.push_back(Img3);
+		frame.clear();
+		}*/
+		imshow("Image", Img);
+		imshow("Image2", readImg);
 		imshow("diffImage", diff);
 		imshow("tmpImage", tmp);
-		imshow("resultImage", res);
-		imshow("Image", Img);
-		imshow("Image2", Img2);
-		imshow("Image3", Img3);
-		imshow("tmp2", result);
-		
+		imshow("resultImage", result);
+
 		if (cvWaitKey() == 27) continue; // break에서 바꿈 키입력받을때마다 프레임이동
-	
+
 	}
-	//checkDegree(src);
-	
-	/*int size = frame[0].rows / 6;
-	thread t1(&checkDegree, frame , 0, size);
-	thread t2(&checkDegree, frame, size, size * 2);
-	thread t3(&checkDegree, frame, size * 2, size * 3);
-	thread t4(&checkDegree, frame, size * 3, size * 4);
-	thread t5(&checkDegree, frame, size * 4, size * 5);
-	thread t6(&checkDegree, frame, size * 5, size * 6);
+
+	int size = frame[0].rows / 6; // thread 생성
+	thread t1(&cal_Degree, frame, 0, size);
+	thread t2(&cal_Degree, frame, size, size * 2);
+	thread t3(&cal_Degree, frame, size * 2, size * 3);
+	thread t4(&cal_Degree, frame, size * 3, size * 4);
+	thread t5(&cal_Degree, frame, size * 4, size * 5);
+	thread t6(&cal_Degree, frame, size * 5, size * 6);
 
 	t1.join();
 	t2.join();
 	t3.join();
 	t4.join();
 	t5.join();
-	t6.join();*/
-
+	t6.join();
 
 	imwrite("result.bmp", des);
 	capture.release();
 	vw.release();
 
-
-	cvDestroyWindow("tt");
-	
 	return 0;
 }
-void checkDegree(vector <Mat> &frame, int start, int end) {  // 단일 구간의 프레임들 , des는 구간의 대표값
+void cal_Degree(vector <Mat> &frame, int start, int end) {  // 단일 구간의 프레임들 , des는 구간의 대표값
+	printf("%d %d %d\n",frame.size(), start, end);
 	Vec3b* desMat = (Vec3b*)des.data;
-	printf("%d %d\n", start, end);
- //	for (int i = 0; i < frame[0].rows; i++) {
 	for (int i = start; i < end; i++) {
 		for (int j = 0; j < frame[0].cols; j++) {
 			vector <int> agrDegree(frame.size(), 0);
@@ -171,12 +183,10 @@ void checkDegree(vector <Mat> &frame, int start, int end) {  // 단일 구간의 프레
 						}
 				 }
 			}
-			int ret = sampleVal(agrDegree);
+			int ret = return_Max(agrDegree);
 			Vec3b* tmp = (Vec3b*)frame[ret].data;
 			desMat[i * frame[0].cols + j] = tmp[i * frame[0].cols + j];
-			//vw.write(des);
 		}
-		//printf("%d\n", i);
 	}
 
 	imwrite("Img.bmp", frame[0]);
@@ -184,7 +194,7 @@ void checkDegree(vector <Mat> &frame, int start, int end) {  // 단일 구간의 프레
 	imwrite("result.bmp", des);
 
 }
-int sampleVal(vector <int> &agrDegree)
+int return_Max(vector <int> &agrDegree) // 일치도 최대값을 가진 프레임 번호 반환
 {
 	int ret = *max_element(agrDegree.begin(), agrDegree.end()); // 일치도 최대 구하기
 
@@ -195,7 +205,7 @@ int sampleVal(vector <int> &agrDegree)
 	return 0;
 }
 
-void cvDiff(Mat &image, Mat &image2,  Mat &diff) {
+void cvDiff(Mat &image, Mat &image2,  Mat &diff) { // 차영상
 	int x, y;
 	for (y = 0; y < image.rows; y++)
 	{
@@ -216,7 +226,7 @@ void cvDiff(Mat &image, Mat &image2,  Mat &diff) {
 }
 
 
-void changeGray(Mat &Img)
+void changeGray(Mat &Img) // 채널 3개 이진화 하기
 {
 	Vec3b* data = (Vec3b*)Img.data;
 	for (int i = 0; i < Img.rows; i++) {
@@ -228,7 +238,7 @@ void changeGray(Mat &Img)
 		}
 	}
 }
-void copy(const Mat &Img, Mat &result, Mat &mask)
+void copy(const Mat &Img, Mat &result, Mat &mask) // copyTo 구현
 {
 	Vec3b* maskData = (Vec3b*)mask.data;
 	Vec3b* ImgData = (Vec3b*)Img.data;
