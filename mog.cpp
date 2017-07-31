c4462b841743a87ac2fd6b888b0ea2e357964abb
이유정 <dnfldml3476@gmail.com>
2017-07-31 15:19:23 +0900

    background 추출 시 Mat연산에 있어서 Integer형으로 변환 후 연산 뒤 unchar형의 Mat에 집어 넣도록 수정

--- mog.cpp
+++ mog.cpp
@@ -26,9 +26,9 @@ using namespace std;
 int sub_Bground(char *videoFile);
 void changeGray(Mat &Img);
 void cvDiff(Mat &image, Mat &image2, Mat &diff);
-void copy(const Mat &Img, Mat &result, Mat &mask);
-void cal_Degree(vector <Mat> &frame, int start, int end); 
-int return_Max(vector <int> &agrDegree); 
+void copy(Mat &Img, Mat &result, Mat &mask);
+void cal_Degree(vector <Mat> &frame, int start, int end);
+int return_Max(vector <int> &agrDegree);
 void gray(char *videoFile);
 
 Mat des;
@@ -40,14 +40,14 @@ int main(int argc, char *argv[])
 	//cvNamedWindow("tmpImage");
 	//cvNamedWindow("diffImage");
 	//cvNamedWindow("resultImage");
-	//if (sub_Bground("3.mp4") == 1)
-	//	return 1;
-	//if (cvWaitKey() == 27) 
-	//	; // break에서 바꿈 키입력받을때마다 프레임이동
+	if (sub_Bground("3.mp4") == 1)
+		return 1;
+	if (cvWaitKey() == 27)
+		//	; // break에서 바꿈 키입력받을때마다 프레임이동
 
-	gray("3.mp4");
-	cvDestroyWindow("tt");
-	
+		/*gray("3.mp4");*/
+		cvDestroyWindow("tt");
+
 	return 0;
 }
 void gray(char *videoFile)
@@ -58,15 +58,33 @@ void gray(char *videoFile)
 		cerr << "Unable to open video file: " << "tt" << endl;
 		exit(EXIT_FAILURE);
 	}
-	Mat Img = imread("result.bmp", IMREAD_GRAYSCALE);
-	Mat diff = Mat(Img.rows, Img.cols, CV_8UC1, Scalar(0));
-	Mat readImg = Mat(Img.rows, Img.cols, CV_8UC1, Scalar(0));
-	Mat tmp = Mat(Img.rows, Img.cols, CV_8UC1, Scalar(0));
+	Mat Img = imread("result.bmp");//, IMREAD_GRAYSCALE);
+	Mat diff = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0));
+	Mat readImg = Mat(Img.rows, Img.cols, CV_16UC3, Scalar(0, 0, 0));
+	Mat tmp = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0));
+
+	Ptr<BackgroundSubtractor> pMOG2; //MOG2 Background subtractor
+	pMOG2 = createBackgroundSubtractorMOG2();
 
 	while (capture.read(readImg)) {
-		cvtColor(readImg, readImg, CV_BGR2GRAY);
+		//cvtColor(readImg, readImg, CV_BGR2GRAY);
+		//	pMOG2->apply(readImg, tmp);
+		//get the frame number and write it on the current frame
+		stringstream ss;
+		rectangle(readImg, cv::Point(10, 2), cv::Point(100, 20),
+			cv::Scalar(255, 255, 255), -1);
+		ss << capture.get(CAP_PROP_POS_FRAMES);
+		string frameNumberString = ss.str();
+		putText(readImg, frameNumberString.c_str(), cv::Point(10, 10),
+			FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
+		//show the current frame and the fg masks
+		imshow("Frame", readImg);
+		imshow("FG Mask MOG 2", tmp);
+		//get the input from the keyboard
+
 		absdiff(Img, readImg, diff); // 첫 프레임과 매 프레임마다의 차영상을 diff에 저장
-		threshold(diff, tmp, 30, 255, CV_THRESH_BINARY); // 차영상을 이진화 시켜서 tmp에 저장
+		threshold(diff, tmp, 20, 255, CV_THRESH_BINARY); // 차영상을 이진화 시켜서 tmp에 저장
+		changeGray(tmp);
 		erode(tmp, tmp, Mat(), Point(-1, -1), 3);
 		imshow("tmp", tmp);
 		imshow("Img", Img);
@@ -98,10 +116,10 @@ int sub_Bground(char *videoFile)
 	Mat Img;
 	capture.read(Img); // 처프레임을 Img에 저장
 
-	//Mat Img = imread("result.bmp"); // Img에 결과로 얻은 이미지 저장
-	Mat readImg = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0)); // 
-	Mat diff = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0));
-	Mat tmp = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0));
+					   //Mat Img = imread("result.bmp"); // Img에 결과로 얻은 이미지 저장
+	Mat readImg = Mat(Img.rows, Img.cols, CV_32SC3, Scalar(0, 0, 0)); // 
+	Mat diff = Mat(Img.rows, Img.cols, CV_32SC3, Scalar(0, 0, 0));
+	Mat tmp = Mat(Img.rows, Img.cols, CV_32SC3, Scalar(0, 0, 0));
 	des = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0)); // 결과를 저장할 Mat
 
 	Ptr<BackgroundSubtractor> pMOG2; //MOG2 Background subtractor
@@ -114,7 +132,7 @@ int sub_Bground(char *videoFile)
 		check++;
 		if (check % 5 != 0)
 			continue;
-		Mat result = Mat(Img.rows, Img.cols, CV_8UC3, Scalar(0, 0, 0));
+		Mat result = Mat(Img.rows, Img.cols, CV_32SC3, Scalar(0, 0, 0));
 
 		absdiff(Img, readImg, diff); // 첫 프레임과 매 프레임마다의 차영상을 diff에 저장
 		threshold(diff, tmp, 20, 255, CV_THRESH_BINARY); // 차영상을 이진화 시켜서 tmp에 저장
@@ -123,10 +141,11 @@ int sub_Bground(char *videoFile)
 		changeGray(tmp); // 이진화 시킨 tmp를 완벽하게 이진화
 
 		pMOG2->apply(tmp, diff); // 이진화 시킨 것을 가지고 MOG2
-		erode(diff, diff, Mat(), Point(-1, -1), 3); 
+		erode(diff, diff, Mat(), Point(-1, -1), 3);
 		//blur(diff, diff, Size(5, 5)); // noise 제거
 		diff = ~diff;
-		readImg.copyTo(result, diff); // 물체 영역의 반전을 붙여넣는다.
+		//readImg.copyTo(result, diff); // 물체 영역의 반전을 붙여넣는다.
+		copy(readImg, result, diff); // 물체 영역의 반전을 붙여넣는다.
 
 
 		frame.push_back(result);
@@ -168,24 +187,33 @@ int sub_Bground(char *videoFile)
 	return 0;
 }
 void cal_Degree(vector <Mat> &frame, int start, int end) {  // 단일 구간의 프레임들 , des는 구간의 대표값
-	printf("%d %d %d\n",frame.size(), start, end);
+	printf("%d %d %d\n", frame.size(), start, end);
+	vector <Mat> tmp;
+	tmp.resize(frame.size());
+	for (int i = 0; i < frame.size(); i++)
+	{
+		frame[i].convertTo(tmp[i], CV_8UC3);
+	}
 	Vec3b* desMat = (Vec3b*)des.data;
 	for (int i = start; i < end; i++) {
 		for (int j = 0; j < frame[0].cols; j++) {
 			vector <int> agrDegree(frame.size(), 0);
-			for (int k = 0; k < frame.size(); k++) { 
-				 for (int t = k+1; t < frame.size(); t++) {
-						Vec3b* data = (Vec3b*) frame[k].data;
-						Vec3b* data2 = (Vec3b*) frame[t].data;
-						if (abs(data[i * frame[0].cols + j][0] - data2[i * frame[0].cols + j][0]) < 10 && abs(data[i * frame[0].cols + j][1] - data2[i * frame[0].cols + j][1]) < 10 && abs(data[i * frame[0].cols + j][2] - data2[i * frame[0].cols + j][2]) < 10) { // DEGREE보다 작으면 증가
-							agrDegree[k]++;
-							agrDegree[t]++;
-						}
-				 }
+			for (int k = 0; k < frame.size(); k++) {
+				for (int t = k + 1; t < frame.size(); t++) {
+					Vec3i* data = (Vec3i*)frame[k].data;
+					Vec3i* data2 = (Vec3i*)frame[t].data;
+					if (data[i * frame[0].cols + j] == Vec3i(300, 300, 300)) {
+						continue;
+					}
+					else if (abs(data[i * frame[0].cols + j][0] - data2[i * frame[0].cols + j][0]) < 10 && abs(data[i * frame[0].cols + j][1] - data2[i * frame[0].cols + j][1]) < 10 && abs(data[i * frame[0].cols + j][2] - data2[i * frame[0].cols + j][2]) < 10) { // DEGREE보다 작으면 증가
+						agrDegree[k]++;
+						agrDegree[t]++;
+					}
+				}
 			}
 			int ret = return_Max(agrDegree);
-			Vec3b* tmp = (Vec3b*)frame[ret].data;
-			desMat[i * frame[0].cols + j] = tmp[i * frame[0].cols + j];
+			Vec3b* tmpData = (Vec3b*)tmp[ret].data;
+			desMat[i * frame[0].cols + j] = tmpData[i * frame[0].cols + j];
 		}
 	}
 
@@ -205,7 +233,7 @@ int return_Max(vector <int> &agrDegree) // 일치도 최대값을 가진 프레임 번호 반환
 	return 0;
 }
 
-void cvDiff(Mat &image, Mat &image2,  Mat &diff) { // 차영상
+void cvDiff(Mat &image, Mat &image2, Mat &diff) { // 차영상
 	int x, y;
 	for (y = 0; y < image.rows; y++)
 	{
@@ -238,15 +266,21 @@ void changeGray(Mat &Img) // 채널 3개 이진화 하기
 		}
 	}
 }
-void copy(const Mat &Img, Mat &result, Mat &mask) // copyTo 구현
+void copy(Mat &Img, Mat &result, Mat &mask) // copyTo 구현
 {
-	Vec3b* maskData = (Vec3b*)mask.data;
-	Vec3b* ImgData = (Vec3b*)Img.data;
-	Vec3b* resData = (Vec3b*)result.data;
+	Img.convertTo(Img, CV_32SC3);
+
+	uchar* maskData = (uchar*)mask.data;
+	Vec3i* ImgData = (Vec3i*)Img.data;
+	Vec3i* resData = (Vec3i*)result.data;
+
 	for (int i = 0; i < mask.rows; i++) {
 		for (int j = 0; j < mask.cols; j++) {
-			if (maskData[i * mask.cols + j] == Vec3b(255, 255, 255)) { // 흰색이라면! 붙이기
+			if (maskData[i * mask.cols + j] != uchar(128)) { // 흰색이라면! 붙이기
 				resData[i * mask.cols + j] = ImgData[i * mask.cols + j];
+			}
+			else { // 검은색 부분은 300으로 채우기
+				resData[i * mask.cols + j] = Vec3i(300, 300, 300);
 			}
 		}
 	}
@@ -254,41 +288,41 @@ void copy(const Mat &Img, Mat &result, Mat &mask) // copyTo 구현
 /*
 void Mat::copyTo(OutputArray _dst, InputArray _mask) const
 {
-	Mat mask = _mask.getMat();
-	if (!mask.data)
-	{
-		copyTo(_dst);
-		return;
-	}
+Mat mask = _mask.getMat();
+if (!mask.data)
+{
+copyTo(_dst);
+return;
+}
 
-	int cn = channels(), mcn = mask.channels();
-	CV_Assert(mask.depth() == CV_8U && (mcn == 1 || mcn == cn));
-	bool colorMask = mcn > 1;
+int cn = channels(), mcn = mask.channels();
+CV_Assert(mask.depth() == CV_8U && (mcn == 1 || mcn == cn));
+bool colorMask = mcn > 1;
 
-	size_t esz = colorMask ? elemSize1() : elemSize();
-	BinaryFunc copymask = getCopyMaskFunc(esz);
+size_t esz = colorMask ? elemSize1() : elemSize();
+BinaryFunc copymask = getCopyMaskFunc(esz);
 
-	uchar* data0 = _dst.getMat().data;
-	_dst.create(dims, size, type());
-	Mat dst = _dst.getMat();
+uchar* data0 = _dst.getMat().data;
+_dst.create(dims, size, type());
+Mat dst = _dst.getMat();
 
-	if (dst.data != data0) // do not leave dst uninitialized
-		dst = Scalar(0);
+if (dst.data != data0) // do not leave dst uninitialized
+dst = Scalar(0);
 
-	if (dims <= 2)
-	{
-		CV_Assert(size() == mask.size());
-		Size sz = getContinuousSize(*this, dst, mask, mcn);
-		copymask(data, step, mask.data, mask.step, dst.data, dst.step, sz, &esz);
-		return;
-	}
+if (dims <= 2)
+{
+CV_Assert(size() == mask.size());
+Size sz = getContinuousSize(*this, dst, mask, mcn);
+copymask(data, step, mask.data, mask.step, dst.data, dst.step, sz, &esz);
+return;
+}
 
-	const Mat* arrays[] = { this, &dst, &mask, 0 };
-	uchar* ptrs[3];
-	NAryMatIterator it(arrays, ptrs);
-	Size sz((int)(it.size*mcn), 1);
+const Mat* arrays[] = { this, &dst, &mask, 0 };
+uchar* ptrs[3];
+NAryMatIterator it(arrays, ptrs);
+Size sz((int)(it.size*mcn), 1);
 
-	for (size_t i = 0; i < it.nplanes; i++, ++it)
-		copymask(ptrs[0], 0, ptrs[2], 0, ptrs[1], 0, sz, &esz);
+for (size_t i = 0; i < it.nplanes; i++, ++it)
+copymask(ptrs[0], 0, ptrs[2], 0, ptrs[1], 0, sz, &esz);
 }
 */
