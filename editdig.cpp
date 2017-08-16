849f425fb99eba2be32696b495e3e25d3be9d782
DESKTOP-ORFO5BQ\방주성 <bj2419@naver.com>
2017-08-16 14:44:35 +0900

    click event complete

--- editdig.cpp
+++ editdig.cpp
@@ -2,6 +2,8 @@
 #include <qgraphicsview.h>
 #include <QDirIterator>
 #include <iostream>
+#include<qstring.h>
+#include<stdlib.h>
 editDig::editDig(QWidget * parent) : QWidget(parent) {
 	ui.setupUi(this);
 	dw = new QDesktopWidget();
@@ -41,71 +43,70 @@ void editDig::addImgItem() {
 	while (iterDir.hasNext())
 	{
 		iterDir.next();
-		list = new QListWidgetItem(QPixmap(iterDir.filePath()), "");
+		list = new QListWidgetItem(QPixmap(iterDir.filePath()), NULL);
 		list->setFlags(Qt::ItemIsEditable | Qt::ItemIsEnabled);
 		ui.listWidget->addItem(list);
-
 	}
-	
-
-	/*int col = 0, row = 0, tem = 0;
-	QDir dir = QDir("imgData");
-	QTableWidgetItem *thumbnail;
-	QString str2[10][5];
-	QFileInfoList list = dir.entryInfoList();
-
-	for (int i = 0; i < list.size(); ++i) {
-		QFileInfo fileInfo = list.at(i);
-
-		if ((fileInfo.fileName().toStdString().compare(".") == 0) || (fileInfo.fileName().toStdString().compare("..") == 0))
-			continue;
-
-		if (fileInfo.isDir()) {
-			QFileInfoList list2 = QDir(fileInfo.filePath()).entryInfoList();
-			tem = 0;
-			for (int j = 0; j < list2.size(); j++) {
-				QFileInfo info2 = list2.at(j);
-				if (info2.isFile())
-				{
-					str2[row][tem] = info2.filePath();
-					tem++;
-				}
-			}
-			row++;
-			if (col < tem)
-				col = tem;
-		}
-
-	}
-	ui.tableWidget->setRowCount(row);
-	ui.tableWidget->setColumnCount(col);
-
-	for (int u = 0; u < row; u++)
-		for (int t = 0; t < col; t++)
-		{
-			thumbnail = new QTableWidgetItem();
-			thumbnail->setData(Qt::DecorationRole, QPixmap(str2[u][t]));
-			ui.tableWidget->setItem(u, t, thumbnail);
-
-		}
-
-
-	for (int u = 0; u < row; u++)
-		ui.tableWidget->setRowHeight(u, (height / row));
-
-	for (int u = 0; u < row; u++)
-		ui.tableWidget->setColumnWidth(u, (width / col));
-*/
 }
+
+/*int col = 0, row = 0, tem = 0;
+QDir dir = QDir("imgData");
+QTableWidgetItem *thumbnail;
+QString str2[10][5];
+QFileInfoList list = dir.entryInfoList();
+
+for (int i = 0; i < list.size(); ++i) {
+	QFileInfo fileInfo = list.at(i);
+
+	if ((fileInfo.fileName().toStdString().compare(".") == 0) || (fileInfo.fileName().toStdString().compare("..") == 0))
+		continue;
+
+	if (fileInfo.isDir()) {
+		QFileInfoList list2 = QDir(fileInfo.filePath()).entryInfoList();
+		tem = 0;
+		for (int j = 0; j < list2.size(); j++) {
+			QFileInfo info2 = list2.at(j);
+			if (info2.isFile())
+			{
+				str2[row][tem] = info2.filePath();
+				tem++;
+			}
+		}
+		row++;
+		if (col < tem)
+			col = tem;
+	}
+
+}
+ui.tableWidget->setRowCount(row);
+ui.tableWidget->setColumnCount(col);
+
+for (int u = 0; u < row; u++)
+	for (int t = 0; t < col; t++)
+	{
+		thumbnail = new QTableWidgetItem();
+		thumbnail->setData(Qt::DecorationRole, QPixmap(str2[u][t]));
+		ui.tableWidget->setItem(u, t, thumbnail);
+
+	}
+
+
+for (int u = 0; u < row; u++)
+	ui.tableWidget->setRowHeight(u, (height / row));
+
+for (int u = 0; u < row; u++)
+	ui.tableWidget->setColumnWidth(u, (width / col));
+*/
 
 void editDig::checkItems(QListWidgetItem *item)
 {
-	QPixmap buf = item->icon().pixmap(QSize(32, 32));
+	int n = ui.listWidget->row(item);
+	/*QPixmap buf = item->icon().pixmap(QSize(32, 32));
 	ui.label->setPixmap(buf);
+*/
+	ui.label->setText(""+n);
+	std::cout << n;
 
-	
-//	if(item->isSelected())
-	//std::cout << (item->text()).toStdString() << std::endl;
 }
 
 void editDig::enterItem(QTableWidgetItem * item) {
